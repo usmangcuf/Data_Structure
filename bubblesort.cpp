9fbe8828638986af67518d7130b0363b02651b5e
usmangcuf <email2usman@gmail.com>
2026-09-19 23:33:16 +0500

    bubblesort added

---
+++ bubblesort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)
+    {
+        for (j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+int main()
+{
+
+    int i, a[5];
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Enter the number: " << i;
+        cin >> a[i];
+    }
+    insertionSort(a, 5);
+    for (i = 0; i < 5; i++)
+    {
+        cout << "a[" << i << "]" << a[i] << endl;
+    }
+
+    return 0;
+}
